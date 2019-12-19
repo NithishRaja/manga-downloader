31f49c84fa5bd7fb6a7778ec5199d2bf36e2edd6
nithish <nithish98raja@gmail.com>
2019-12-19 17:33:46 +0530

    add code to get chapter limit

--- bnha.py
+++ bnha.py
@@ -4,11 +4,12 @@
 #
 
 # Dependencies
-import requests
-import bs4
 import os
 import sys
 import threading
+import re
+import requests
+import bs4
 
 # Set base dir location
 location = os.path.join(os.getcwd(), "BNHA")
@@ -17,8 +18,18 @@ location = os.path.join(os.getcwd(), "BNHA")
 if not os.path.exists(location):
     os.makedirs(location)
 
-# Set latest chapter
-latestChapter = int(sys.argv[1]) if len(sys.argv)>1 else 2
+# Function to get latest chapter
+def getLatestChapter():
+    # Set latest chapter search string
+    latestChapterSearchString = re.compile(r'\d+')
+    # Send request to get list of all chapters
+    res = requests.get("https://myheromanga.com/")
+    # Parse HTML
+    resHTML = bs4.BeautifulSoup(res.text, features="html.parser")
+    # Get list containing all chapter numbers
+    list = resHTML.select("#Chapters_List a")
+    # Return latest chapter
+    return latestChapterSearchString.search(list[0].text).group()
 
 # Function to get pages for given chapter
 def getPages(chapterNo):
@@ -53,11 +64,21 @@ def getPages(chapterNo):
             fileObject.write(chunk)
         fileObject.close()
 
+# Set latest chapter
+latestChapter = int(getLatestChapter())
+
+# Set chapter limit
+chapterLimit = latestChapter
+
+# Check if chapter limit is lesser than latest chapter
+if len(sys.argv) > 1 and latestChapter > int(sys.argv[1]):
+    chapterLimit = int(sys.argv[1])
+
 # Thread array
 threads = ["Dummy"]
 
 # Iterate over each chapter and Initialise a thread to download chapter
-for i in range(1, latestChapter):
+for i in range(1, chapterLimit):
     # Initialise threads for each chapter
     threadObj = threading.Thread(target=getPages, args=[i])
     # Append thread to array
@@ -67,7 +88,7 @@ for i in range(1, latestChapter):
 activeThreadLimit = 5
 
 # Iterato over all threads
-for currentThread in range(1, latestChapter):
+for currentThread in range(1, chapterLimit):
     # Check if current thread is under the limit
     if not currentThread > activeThreadLimit:
         # Current thread is under the limit
