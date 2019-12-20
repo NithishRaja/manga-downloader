9cbd808cc8979a9f8228adca42fa7a1fe67b35da
nithish <nithish98raja@gmail.com>
2019-12-20 20:07:31 +0530

    refactor code to download chapter only if it doesn't exist

--- bnha.py
+++ bnha.py
@@ -34,33 +34,36 @@ def getChapterList():
 
 # Function to get pages for given chapter
 def getPages(chapterNo):
-    # res = requests.get("https://myheromanga.com/manga/boku-no-hero-academia-chapter-"+str(chapterNo))
+    # Set chapter file name
+    fileName = os.path.join(location, "Chapter "+str(chapterNo+1)+".pdf")
+
+    # Check if chapter already exists
+    if os.path.exists(fileName):
+        # Chapter already exists, return
+        return
+
+    # Get chapter pages list
     res = requests.get(chapterList[chapterNo].attrs["href"])
     # Getting response HTML
     resHTML = bs4.BeautifulSoup(res.text, features="html.parser")
-
     # Getting only the links
     links = resHTML.select('img[src]')
 
     # Initialise pages array
     pages = []
-
     # Iterate over each image
     for currentPage in range(len(links)):
         # Get image URL
         imageURL = links[currentPage].attrs["src"]
-
         # Get image from its URL
         image = requests.get(imageURL)
-
         # Append image to pages array
         pages.append(image.content)
 
     # Convert images into pdf format
     pdf_bytes = img2pdf.convert(pages)
-
     # Write to pdf
-    fileObject = open(os.path.join(location, "Chapter "+str(chapterNo+1)+".pdf"), "wb")
+    fileObject = open(fileName, "wb")
     fileObject.write(pdf_bytes)
     fileObject.close()
 
