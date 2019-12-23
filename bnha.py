b995cb2370069b276cda03317e23ac914b620224
nithish <nithish98raja@gmail.com>
2019-12-23 08:39:37 +0530

    refactor code to store all global variables in state object

--- bnha.py
+++ bnha.py
@@ -13,17 +13,32 @@ import requests
 import bs4
 import img2pdf
 
-# Set base dir location
-location = os.path.join(os.getcwd(), "BNHA")
+# Initialise a state object
+state = {
+    # Set base dir location
+    "location" : os.path.join(os.getcwd(), "BNHA"),
+    # Set base URL
+    "baseURL" : "https://myheromanga.com/",
+    # Set active thread limit
+    "activeThreadLimit" : 5,
+    # Initialise threads array
+    "threads" : [],
+    # Initialise latest chapter
+    "latestChapter" : 1,
+    # Initialise chapter list
+    "chapterList" : {},
+    #Initialise chapter limit
+    "chapterLimit" : 1
+}
 
 # Make new dir
-if not os.path.exists(location):
-    os.makedirs(location)
+if not os.path.exists(state["location"]):
+    os.makedirs(state["location"])
 
 # Function to get chapters list and latest chapter number
 def getChapterList():
     # Send request to get list of all chapters
-    res = requests.get("https://myheromanga.com/")
+    res = requests.get(state["baseURL"])
     # Set latest chapter search string
     chapterNoSearchString = re.compile(r'\d+(\.\d+)?')
     # Parse HTML
@@ -33,20 +48,16 @@ def getChapterList():
     # Reverse list to have first chapter as first element
     chapterListHTML.reverse()
     # Get latest chapter
-    latestChapter = int(chapterNoSearchString.search(chapterListHTML[-1].text).group())
-    # Initialise chapter dict
-    chapterList = {}
+    state["latestChapter"] = int(chapterNoSearchString.search(chapterListHTML[-1].text).group())
     # Iteraate over all chapters
     for chapter in chapterListHTML:
         # Append element to chapter list
-        chapterList[chapterNoSearchString.search(chapter.text).group()] = chapter.attrs["href"]
-    # Return latest chapter
-    return [chapterList, latestChapter]
+        state["chapterList"][chapterNoSearchString.search(chapter.text).group()] = chapter.attrs["href"]
 
 # Function to get pages for given chapter
 def getPages(chapterNo):
     # Set chapter file name
-    fileName = os.path.join(location, "Chapter "+chapterNo+".pdf")
+    fileName = os.path.join(state["location"], "Chapter "+chapterNo+".pdf")
 
     # Check if chapter already exists
     if os.path.exists(fileName):
@@ -54,7 +65,7 @@ def getPages(chapterNo):
         return
 
     # Get chapter pages list
-    res = requests.get(chapterList[chapterNo])
+    res = requests.get(state["chapterList"][chapterNo])
     # Getting response HTML
     resHTML = bs4.BeautifulSoup(res.text, features="html.parser")
     # Getting only the links
@@ -79,22 +90,19 @@ def getPages(chapterNo):
     fileObject.close()
 
 # Call function to get chapter list
-chapterList, latestChapter = getChapterList()
+getChapterList()
 
 # Set chapter limit
-chapterLimit = latestChapter
+state["chapterLimit"] = state["latestChapter"]
 
 # Check if chapter limit is lesser than latest chapter
-if len(sys.argv) > 1 and latestChapter > int(sys.argv[1]):
-    chapterLimit = int(sys.argv[1])
-
-# Thread array
-threads = []
+if len(sys.argv) > 1 and state["latestChapter"] > int(sys.argv[1]):
+    state["chapterLimit"] = int(sys.argv[1])
 
 # Iterate over each chapter
-for key in chapterList.keys():
+for key in state["chapterList"].keys():
     # Check if current key is over chapter limit
-    if int(round(float(key))) > chapterLimit:
+    if int(round(float(key))) > state["chapterLimit"]:
         # Current key is greater than chapter limit
         # Break out of loop
         break;
@@ -102,15 +110,12 @@ for key in chapterList.keys():
         # Initialise threads for each chapter
         threadObj = threading.Thread(target=getPages, args=[key])
         # Append thread to array
-        threads.append(threadObj)
-
-# Set active thread limit
-activeThreadLimit = 5
+        state["threads"].append(threadObj)
 
 # Iterate over all threads
-for currentThread in threads:
+for currentThread in state["threads"]:
     # If no of active threads is greater than active thread limit, wait till a thread finishes execution
-    while threading.active_count() > activeThreadLimit:
+    while threading.active_count() > state["activeThreadLimit"]:
         # Wait for 3 seconds and check again
         time.sleep(3)
     # Start the thread
